eaf5fd2f0f3fa4847d23e20f2fc9025b90672719
Bent Terp <bent.terp@basefarm.com>
2017-03-10 10:20:25 +0100

    IGW

--- igw.tf
+++ igw.tf
@@ -6,4 +6,20 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+    tags {
+    CostCenter = "${var.costcenter}"
+    Name = "${var.nameprefix}"
+    }
+}
+
+resource "aws_route" "public" {
+    route_table_id = "${aws_route_table.public.id"
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+    depends_on = ["aws_route_table.public","aws_internet_gateway.igw"]
+}
+
 output "igwid" { value = "${aws_internet_gateway.igw.id}" }
+output "pubrt" { value ="${aws_route_table.public.id}" }
