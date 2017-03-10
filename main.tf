2641d618d96975c32374be39b2c3dadcd4bc5bca
Bent Terp <bent.terp@basefarm.com>
2017-03-10 09:40:10 +0100

    First push

---
+++ main.tf
@@ -0,0 +1,25 @@
+#variable "" { default= type="string" }
+variable "region" { type="string" }
+variable "costcenter" { type="string" }
+variable "netaddr" { default="10.0.0.0" type="string" }
+variable "netsize" { default="24" type="string" }
+variable "azcount" { default="3" type="string" }
+
+data "null_data_source" "my" {
+  inputs = {
+    subsize = "${max( 22, min(var.netsize+4, 28))}"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "${var.netaddr}/${var.netsize}"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags {
+    CostCenter = "${var.costcenter}"
+  }
+}
+
+output "subsize" { value = "${data.null_data_source.my.inputs["subsize"] }" }
+output "vpcid" { value = "${aws_vpc.main.id}" }
