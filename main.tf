a56b28e81368194d32f51855f164cf2c8890d08d
Bent Terp <bent.terp@basefarm.com>
2017-03-10 12:58:15 +0100

    NAT

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ variable "azcount" { default="3" type="string" }
 data "null_data_source" "my" {
   inputs = {
     newbits = "${max(1 , max( 22 , min(var.netsize + 4 , 28)) - var.netsize)}"
-#    azs = "${length(data.aws_availability_zones.available.id)}"
-    azs = "${data.aws_availability_zones.available.id}"
+    azs = "${length(data.aws_availability_zones.available.names)}"
+#    azs = "${data.aws_availability_zones.available.names}"
   }
 }
 
