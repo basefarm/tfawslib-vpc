0883f28524920b3978d319639fa2190b9f3ecc99
Bent Terp <bent.terp@basefarm.com>
2017-03-10 10:22:03 +0100

    IGW

--- igw.tf
+++ igw.tf
@@ -9,8 +9,8 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "public" {
   vpc_id = "${aws_vpc.vpc.id}"
     tags {
-    CostCenter = "${var.costcenter}"
-    Name = "${var.nameprefix}"
+      CostCenter = "${var.costcenter}"
+      Name = "${var.nameprefix}"
     }
 }
 
@@ -18,8 +18,8 @@ resource "aws_route" "public" {
     route_table_id = "${aws_route_table.public.id"
     destination_cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.igw.id}"
-    depends_on = ["aws_route_table.public","aws_internet_gateway.igw"]
+#    depends_on = ["aws_route_table.public","aws_internet_gateway.igw"]
 }
 
 output "igwid" { value = "${aws_internet_gateway.igw.id}" }
-output "pubrt" { value ="${aws_route_table.public.id}" }
+output "pubrt" { value = "${aws_route_table.public.id}" }
