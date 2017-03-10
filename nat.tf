0cc2844b31b40e97f008d97d19105f3928d262a5
Bent Terp <bent.terp@basefarm.com>
2017-03-10 12:15:08 +0100

    NAT

--- nat.tf
+++ nat.tf
@@ -1,10 +1,6 @@
 resource "aws_eip" "nat" {
   count = "${var.azcount}"
   vpc      = true
-#  tags {
-#    CostCenter = "${var.costcenter}"
-#    Name = "${var.nameprefix}"
-#  }
 }
 
 resource "aws_nat_gateway" "natgw" {
@@ -12,10 +8,6 @@ resource "aws_nat_gateway" "natgw" {
   subnet_id = "${element(aws_subnet.dmz.*.id, count.index)}"
   allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
   depends_on = ["aws_internet_gateway.igw"]
-#  tags {
-#    CostCenter = "${var.costcenter}"
-#    Name = "${var.nameprefix}"
-#  }
 }
 
 resource "aws_route_table" "nat" {
@@ -27,16 +19,9 @@ resource "aws_route_table" "nat" {
   }
   tags {
     CostCenter = "${var.costcenter}"
-    Name = "${var.nameprefix}"
+    Name = "${var.nameprefix}-NAT$count.index"
   }
 }
-
-#resource "aws_route" "public" {
-#  count = "${var.azcount}"
-#  route_table_id = "${element(aws_route_table.public.id, count.index)}"
-#  destination_cidr_block = "0.0.0.0/0"
-#  gateway_id = "${element(aws_internet_gateway.igw.id, count.index)}"
-#}
 
 output "nat_routing_tables" { value = "${list(aws_route_table.nat.*.id)}" }
 output "nat_public_ips" { value = "${list(aws_eip.nat.*.public_ip)}" }
