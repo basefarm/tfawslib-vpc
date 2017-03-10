ff02965478a45e11d619f7a6231297a880ffda66
Bent Terp <bent.terp@basefarm.com>
2017-03-10 11:05:04 +0100

    DMZ

--- dmz.tf
+++ dmz.tf
@@ -1,13 +1,12 @@
 resource "aws_subnet" "dmz" {
-#  count = "${var.azcount}"
+  count = "${var.azcount}"
   vpc_id = "${aws_vpc.vpc.id}"
 #  count = "${var.azcount}"
 #  count = 3
 #  cidr_block = "${cidrsubnet(${aws_vpc.vpc.cidr_block},${data.null_data_source.my.inputs["newbits"]},$count.index+0)}"
-#  availability_zone = "${data.aws_availability_zones.available[$count.index]}"
 #  cidr_block = "${cidrsubnet(${aws_vpc.vpc.cidr_block},${data.null_data_source.my.inputs["newbits"]},0)}"
-  cidr_block = "${cidrsubnet(aws_vpc.vpc.cidr_block,data.null_data_source.my.inputs["newbits"],0)}"
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  cidr_block = "${cidrsubnet(aws_vpc.vpc.cidr_block,data.null_data_source.my.inputs["newbits"],$count.index + 0)}"
+  availability_zone = "${data.aws_availability_zones.available.names[$count.index]}"
   tags {
     CostCenter = "${var.costcenter}"
     Name = "${var.nameprefix}-dmz${count.index}"
