15b763f8d9eb7c9e695f6b8f27578835f52f47b8
Bent Terp <bent.terp@basefarm.com>
2017-03-10 10:22:52 +0100

    IGW

--- igw.tf
+++ igw.tf
@@ -15,10 +15,10 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route" "public" {
-    route_table_id = "${aws_route_table.public.id"
+    route_table_id = "${aws_route_table.public.id}"
     destination_cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.igw.id}"
-#    depends_on = ["aws_route_table.public","aws_internet_gateway.igw"]
+    depends_on = ["aws_route_table.public","aws_internet_gateway.igw"]
 }
 
 output "igwid" { value = "${aws_internet_gateway.igw.id}" }
