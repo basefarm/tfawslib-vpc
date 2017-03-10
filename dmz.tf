ac60f6710439651afc22fc4ca9a21a53a9c8fffd
Bent Terp <bent.terp@basefarm.com>
2017-03-10 11:28:39 +0100

    DMZ

--- dmz.tf
+++ dmz.tf
@@ -1,6 +1,8 @@
 resource "aws_subnet" "dmz" {
   count = "${var.azcount}"
   vpc_id = "${aws_vpc.vpc.id}"
+  map_public_ip_on_launch = false
+  
 #  count = "${var.azcount}"
 #  count = 3
 #  cidr_block = "${cidrsubnet(${aws_vpc.vpc.cidr_block},${data.null_data_source.my.inputs["newbits"]},$count.index+0)}"
@@ -9,6 +11,13 @@ resource "aws_subnet" "dmz" {
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   tags {
     CostCenter = "${var.costcenter}"
-    Name = "${var.nameprefix}-dmz${count.index}"
+    Name = "${var.nameprefix}-DMZ${count.index}"
   }
 }
+
+resource "aws_route_table_association" "dmz" {
+  count = "${var.azcount}"
+  subnet_id = "${aws_subnet.dmz.$count.index.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
