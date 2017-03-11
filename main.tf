6e792f76e0246aee5fdc0b10299df7be48c1bce3
Bent Terp <bent.terp@basefarm.com>
2017-03-11 08:15:07 +0100

    AZ

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ variable "costcenter" { type="string" }
 variable "nameprefix" { type="string" }
 variable "netaddr" { default="10.0.0.0" type="string" }
 variable "netsize" { default="24" type="string" }
-variable "azcount" { default="3" type="string" }
+variable "azs" { default="3" type="string" }
 
 data "null_data_source" "my" {
   inputs = {
     newbits = "${max(1 , max( 22 , min(var.netsize + 4 , 28)) - var.netsize)}"
-    azs = "${max( 1 , min( length(data.aws_availability_zones.available.names) , var.azcount) )}"
-    azlist = "${join(",",slice(data.aws_availability_zones.available.names , 0 , max( 1 , min( length(data.aws_availability_zones.available.names) , var.azcount) ) ))}"
+    azcount = "${max( 1 , min( length(data.aws_availability_zones.available.names) , var.azs) )}"
+    azlist = "${join(",",slice(data.aws_availability_zones.available.names , 0 , max( 1 , min( length(data.aws_availability_zones.available.names) , var.azs) ) ))}"
   }
 }
 
@@ -18,6 +18,6 @@ data "aws_availability_zones" "available" {}
 
 
 output "newbits" { value = "${data.null_data_source.my.inputs["newbits"] }" }
-output "azs" { value = "${data.null_data_source.my.inputs["azs"] }" }
+output "azcount" { value = "${data.null_data_source.my.inputs["azcount"] }" }
 
 
